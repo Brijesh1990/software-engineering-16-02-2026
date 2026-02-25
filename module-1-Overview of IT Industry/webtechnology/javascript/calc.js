19d044d9a16f9dd0a29b6b5fbb564bd3d138b6ca
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-02-25 14:16:45 +0530

    25-02-2026 all data uploaded

---
+++ module-1-Overview of IT Industry/webtechnology/javascript/calc.js
@@ -0,0 +1,34 @@
+function add()
+{
+    var a=parseInt(prompt('Enter a values :'));
+    var b=parseInt(prompt('Enter b values :'));
+    var c=a+b
+    alert('The additions of numbers is :'+c)
+
+}
+function subs()
+{
+    var a=parseInt(prompt('Enter a values :'));
+    var b=parseInt(prompt('Enter b values :'));
+    var c=a-b
+    alert('The substractions of numbers is :'+c)
+
+}
+
+function mult()
+{
+    var a=parseInt(prompt('Enter a values :'));
+    var b=parseInt(prompt('Enter b values :'));
+    var c=a*b
+    alert('The multiplications of numbers is :'+c)
+
+}
+
+function dv()
+{
+    var a=parseInt(prompt('Enter a values :'));
+    var b=parseInt(prompt('Enter b values :'));
+    var c=a/b
+    alert('The divisions of numbers is :'+c)
+
+}
---
+++ module-1-Overview of IT Industry/webtechnology/javascript/what is javascript.md
@@ -0,0 +1,7 @@
+# what is javascript ?
+  A javascript is a client side scripting language 
+  A javascript saved with .js file 
+  A javascript used as a programming language 
+  A javascript called via <script> tag 
+
+  
